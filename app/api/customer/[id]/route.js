999c91269814eaccbb6d9c9a376fe1c6b72a9003
ApP0D <u6510550@au.edu>
2024-10-11 11:16:55 +0700

    fix update for [id]

--- app/api/customer/[id]/route.js
+++ app/api/customer/[id]/route.js
@@ -18,3 +18,24 @@ export async function GET(request, { params }) {
     return new Response("Internal Server Error", { status: 500 });
   }
 }
+
+export async function PUT(req, { params }) {
+    const { id } = params;  // Extract id from params
+    const body = await req.json();
+    const { _id, ...updateData } = body;
+  
+    // Ensure the _id matches the id in the URL
+    if (_id !== id) {
+      return new Response("ID mismatch", { status: 400 });
+    }
+  
+    const customer = await Customer.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+  
+    if (!customer) {
+      return new Response("Customer not found", { status: 404 });
+    }
+    
+    return Response.json(customer);
+  }
